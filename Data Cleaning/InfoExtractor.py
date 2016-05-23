75bee32d69f8544b2849c9f9bf33fe587e58b3c9
ShiPeng <spone.2014@gmail.com>
2016-05-23 18:34:48 +0800

    Debug InfoExtractor

--- Data Cleaning/InfoExtractor.py
+++ Data Cleaning/InfoExtractor.py
@@ -3,6 +3,10 @@ import os
 
 def Extractor(dir):
     files = os.listdir(dir)
+    if not (os.path.exists("tmp")):
+        os.mkdir("tmp")
+    if not (os.path.exists("tmp/doc")):
+        os.mkdir("tmp/doc")
     titleFile = open("tmp/title","w")
     for name in files:
         doc = open(dir+"/"+name)
@@ -10,7 +14,7 @@ def Extractor(dir):
         output = open("tmp/doc/"+name.replace(".html",""),"w")
         titleFile.write(name+" : "+content[0].replace("&lt;","<"))
         output.write("Title: "+content[0].replace("&lt;","<"))
-        output.write("ID: "+name.replace(".html",""))
+        output.write("ID: "+name.replace(".html","")+"\n")
         for i in range(len(content)):
             content[i] = " "+content[i].strip()
         output.write("passage: "+"".join(content[1:]))

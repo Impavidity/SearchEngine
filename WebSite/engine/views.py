17a63a5e3f8a3458e35f41af3ad088084b794538
ShiPeng <spone.2014@gmail.com>
2016-06-11 15:35:37 +0800

    main page

--- WebSite/engine/views.py
+++ WebSite/engine/views.py
@@ -1,6 +1,15 @@
 from engine import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
-@app.route('/')
+@app.route('/',methods=["POST","GET"])
 def index():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+    if request.method == "POST":
+        queryWords = request.form["iQuery"]
+        
+        return redirect(url_for("result"))
+
+@app.route('/result', methods=["GET","POST"])
+def result():
+    return render_template("result.html")
